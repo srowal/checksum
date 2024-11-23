16126d0bacd24486fd64ad7987602e7ec79315e6
Subhanshu <107223654+srowal@users.noreply.github.com>
2024-11-23 17:58:54 -0500

    Create sender.s

---
+++ sender.s
@@ -0,0 +1,33 @@
+Input
+Store n1
+Output
+Input
+Store n2
+Output
+Input
+Store n3
+Output
+Input
+Store n4
+Output
+Add n1
+Add n2
+Add n3
+loop, Store sum
+Subt max
+Skipcond 000
+Jump loop
+Load sum
+Output
+Clear
+Subt sum
+Add max
+Output
+Halt
+n1, DEC 0
+n2, DEC 0
+n3, DEC 0
+
+n4, DEC 0
+sum, DEC 0
+max, DEC 256
