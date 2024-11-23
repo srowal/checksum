7919ed493c8576466858bbefd6788989bf9ad492
Subhanshu <107223654+srowal@users.noreply.github.com>
2024-11-23 17:59:21 -0500

    Create receiver.s

---
+++ receiver.s
@@ -0,0 +1,28 @@
+Input
+Store n1
+Input
+Store n2
+Input
+Store n3
+Input
+Store n4
+Input
+Store checkSum
+Add n1
+Add n2
+Add n3
+Add n4
+loop, Store sum
+Subt max
+Skipcond 000
+Jump loop
+Load sum
+Output
+Halt
+n1, DEC 0
+n2, DEC 0
+n3, DEC 0
+n4, DEC 0
+checkSum, DEC 0
+sum, DEC 0
+max, DEC 256
